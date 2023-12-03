efcf10cba8c11f205e85f96e712f31c47fb947a4
Anna Saushkina <saushkinanna@gmail.com>
2023-12-03 15:15:22 +0300

    refactor: deploy

--- src/js/components/scroll.js
+++
@@ -1,9 +0,0 @@
-let headerScroll = document.querySelector(".header").classList;
-window.onscroll = function (e) {
-  if (this.oldScroll > this.scrollY) {
-    headerScroll.remove("hide");
-  } else {
-    headerScroll.add("hide");
-  }
-  this.oldScroll = this.scrollY;
-};
